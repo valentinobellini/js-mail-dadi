f83b62fb14fc8a6fa1ed0c9c8e492d3a00b5d146
ValentinoBellini <valentinobellini.dev@gmail.com>
2024-12-20 15:53:10 +0100

    feat: js mail

---
+++ MAIL/JS/script.js
@@ -0,0 +1,23 @@
+// crea una lista di mail invitati
+const invitedMail = ["pippo@gmail.com", "carlo@gmail.com", "federica@gmail.com", "camilla@gmail.com", "laura@gmail.com", "sergio@gmail.com",]
+
+// chiedi all'utente di inserire la sua mail
+const userMail = prompt("Inserisci la tua mail")
+
+// variabile che segnala se la mail è stata trovata
+let found = false;
+
+// ciclo per controllare se la mail è presente nella lista degli invitati
+for (let i = 0; i < invitedMail.length; i++) {
+    if (invitedMail[i] === userMail) {
+        console.log("Benvenuto alla festa");
+        found = true; //segnala che la mail utente si trova nella lista
+        break; //esce dal ciclo
+    } 
+}
+
+//verifica se la mail non è stata trovata nella lista
+if (!found) {
+    console.log("Mi spiace ma la tua mail non è presente nell'elenco degli invitati");
+    
+}
