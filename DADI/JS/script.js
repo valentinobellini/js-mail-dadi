9446344daa1321d717b348fc54452aefd0040834
ValentinoBellini <valentinobellini.dev@gmail.com>
2024-12-20 16:25:06 +0100

    feat: js dadi

--- DADI/JS/script.js
+++ DADI/JS/script.js
@@ -1,28 +1,23 @@
-//Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
-//Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 
 
-// crea una lista di mail invitati
-const invitedMail = ["pippo@gmail.com", "carlo@gmail.com", "federica@gmail.com", "camilla@gmail.com", "laura@gmail.com", "sergio@gmail.com",]
+// genera un numero casuale da 1 a 6 per il giocatore
+const playerRoll = Math.floor(Math.random() * 6) + 1;
+console.log(playerRoll);
 
-// chiedi all'utente di inserire la sua mail
-const userMail = prompt("Inserisci la tua mail")
 
-// variabile che segnala se la mail è stata trovata
-let found = false;
+// genera un numero casuale da 1 a 6 per il computer
+const computerRoll = Math.floor(Math.random() * 6) + 1;
+console.log(computerRoll);
 
-// ciclo per controllare se la mail è presente nella lista degli invitati
-for (let i = 0; i < invitedMail.length; i++) {
-    if (invitedMail[i] === userMail) {
-        console.log("Benvenuto alla festa");
-        found = true; //segnala che la mail utente si trova nella lista
-        break; //esce dal ciclo
-    } 
-}
 
-//verifica se la mail non è stata trovata nella lista
-if (!found) {
-    console.log("Mi spiace ma la tua mail non è presente nell'elenco degli invitati");
+//stabilisci chi è il vincitore
+if (playerRoll > computerRoll) {
+    console.log("Il giocatore vince");
+} else if (playerRoll < computerRoll) {
+    console.log("Il computer vince");
+} else {
+    console.log("La partita è pari");
     
 }
